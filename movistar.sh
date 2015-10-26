e898e9e8412325fe7db9653bd6afc361e88f663f
Colerax <colerax@gmail.com>
2015-10-26 18:40:45 +0100

    Update movistar.sh

--- movistar.sh
+++ movistar.sh
@@ -15,7 +15,7 @@ function main() {
 	movistar_ftth "$CW_DIR/files_movistar"
 
 	release_version "chaos_calmer" "15.05"
-	firmware_packages "luci luci-i18n-base-es kmod-bridge kmod-ipt-nathelper-rtsp swconfig bird4 igmpproxy udpxy luci-app-udpxy nfs-utils kmod-fs-nfs kmod-fs-nfs-common kmod-nls-cp437 kmod-nls-cp850 kmod-nls-cp852 kmod-nls-iso8859-15 kmod-nls-utf8 miniupnpd luci-app-upnp tcpdump htop ddns-scripts ddns-scripts_no-ip_com luci-app-ddns kmod-usb-storage kmod-fs-ntfs kmod-fs-ext4 kmod-fs-vfat block-mount kmod-scsi-core e2fsprogs samba36-server luci-app-samba ntfs-3g fdisk kmod-usb2 nfs-kernel-server usbutils kmod-loop kmod-usb-core kmod-usb-ohci kmod-usb-printer luci-proto-ipv6 luci-app-wol ip ipsec-tools kmod-ipsec4 kmod-ipsec6 openssl-util iptables-mod-nat-extra ip6tables iptables-mod-ipsec kmod-ipt-ipsec php5 php5-cgi php5-mod-mysql php5-mod-mysqli"
+	firmware_packages "luci luci-i18n-base-es kmod-bridge kmod-ipt-nathelper-rtsp swconfig bird4 igmpproxy udpxy luci-app-udpxy nfs-utils kmod-fs-nfs kmod-fs-nfs-common kmod-nls-cp437 kmod-nls-cp850 kmod-nls-cp852 kmod-nls-iso8859-15 kmod-nls-utf8 miniupnpd luci-app-upnp tcpdump htop ddns-scripts ddns-scripts_no-ip_com luci-app-ddns kmod-usb-storage kmod-fs-ntfs kmod-fs-ext4 kmod-fs-vfat block-mount kmod-scsi-core e2fsprogs samba36-server luci-app-samba ntfs-3g fdisk kmod-usb2 nfs-kernel-server usbutils kmod-loop kmod-usb-core kmod-usb-ohci kmod-usb-printer luci-proto-ipv6 luci-app-wol ip ipsec-tools kmod-ipsec4 kmod-ipsec6 openssl-util iptables-mod-nat-extra ip6tables iptables-mod-ipsec kmod-ipt-ipsec"
 
 	firmware_files "$CW_DIR/files_movistar/"
 
